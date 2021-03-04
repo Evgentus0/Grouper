ef98effdd3b9cf68035bdec95737e6c9332529a3
Eugene <zerom2016romanenko@gmail.com>
2021-03-04 21:46:52 +0200

    update group entity

--- src/Grouper.Api.Data/Entities/Group.cs
+++ src/Grouper.Api.Data/Entities/Group.cs
@@ -12,9 +12,10 @@ namespace Grouper.Api.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public int? ParentGroupId { get; set; }
+        [ForeignKey("ParentGroupId")]
+        public Group ParentGroup { get; set; }
         public List<Post> Tasks { get; set; }
         public string UsefulContent { get; set; }
-        [ForeignKey("ParentGroupId")]
         public List<Group> ChildGroups { get; set; }
     }
 }
