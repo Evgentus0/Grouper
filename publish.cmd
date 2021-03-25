ca96f1d29b96acc070c23f5902a6c567c92a7f69
Eugene <zerom2016romanenko@gmail.com>
2021-03-26 00:47:07 +0200

    add deployment job

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,4 @@
-dotnet publish -c Release 
-
-docker build -t grouper-8team-api "src\\Grouper.Api.Web\bin\\Release\net5.0\\publish"
+docker build -t grouper-8team-api .
 docker tag grouper-8team-api registry.heroku.com/grouper-8team-api/web
 docker push registry.heroku.com/grouper-8team-api/web
 heroku container:release web --app grouper-8team-api
