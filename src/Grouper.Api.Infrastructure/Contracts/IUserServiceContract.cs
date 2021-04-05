624437927bf29d68eeae39199bcf60287c5e7502
Eugene <zerom2016romanenko@gmail.com>
2021-04-05 19:24:47 +0300

    add contracts for user service adn group service

---
+++ src/Grouper.Api.Infrastructure/Contracts/IUserServiceContract.cs
@@ -0,0 +1,37 @@
+﻿using Grouper.Api.Infrastructure.DTOs;
+using Grouper.Api.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouper.Api.Infrastructure.Contracts
+{
+    [ContractClassFor(typeof(IUserService))]
+    class IUserServiceContract : IUserService
+    {
+        public Task<UserDto> GetInfo(string id)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(id));
+            Contract.Ensures(!string.IsNullOrEmpty(Contract.Result<UserDto>()?.Id));
+
+            return default;
+        }
+
+        public Task<string> SignIn(UserDto userDto)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(userDto.Email) && !string.IsNullOrEmpty(userDto.Password), "email or password is null");
+
+            return default;
+        }
+
+        public Task SignUp(UserDto userDto)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(userDto.Email) && !string.IsNullOrEmpty(userDto.Password));
+
+            return default;
+        }
+    }
+}
--- src/Grouper.Api.Infrastructure/Interfaces/IUserService.cs
+++ src/Grouper.Api.Infrastructure/Interfaces/IUserService.cs
@@ -1,6 +1,8 @@
-﻿using Grouper.Api.Infrastructure.DTOs;
+﻿using Grouper.Api.Infrastructure.Contracts;
+using Grouper.Api.Infrastructure.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Grouper.Api.Infrastructure.Interfaces
 {
+    [ContractClass(typeof(IUserServiceContract))]
     public interface IUserService
     {
         Task<string> SignIn(UserDto userDto);
